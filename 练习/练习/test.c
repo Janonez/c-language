2c2e136491c37509559b267914a80579a4a3298a
Janonez <95027402+sharkmini@users.noreply.github.com>
2022-09-25 00:08:22 +0800

    c-language
    code

--- 练习/练习/test.c
+++ 练习/练习/test.c
@@ -156,29 +156,56 @@
 // 输入一个整数序列，判断是否是有序序列，
 // 有序，指序列中的整数从小到大排序或者从大到小排序(相同元素也视为有序)。
 // 数据范围：3≤n≤50  序列中的值都满足 1≤val≤100
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[50] = { 0 };
+//	int i = 0;
+//	int flag1 = 0;// 判断是否为升序
+//	int flag2 = 0;// 判断是否为降序
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		if (i > 0)
+//		{
+//			if (arr[i] < arr[i - 1])
+//				flag1 = 1;
+//			else if (arr[i] > arr[i - 1])
+//				flag2 = 1;
+//		}
+//	}
+//	if (flag1 + flag2 <= 1)
+//		printf("sorted");
+//	else
+//		printf("unsorted");
+//	return 0;
+//}
+
+
+// 输入n个成绩，换行输出n个成绩中最高分数和最低分数的差。
 #include <stdio.h>
 int main()
 {
-	int n = 0;
-	scanf("%d", &n);
-	int arr[] = { 0 };
-	int i = 0;
-	int frag1 = 0;// 判断是否为升序
-	int frag2 = 0;// 判断是否为降序
-	for (i = 0; i < n; i++)
-	{
-		scanf("%d", arr[i]);
-		if (i > 0)
-		{
-			if (arr[i] < arr[i - 1])
-				frag1 = 1;
-			else if (arr[i] > arr[i - 1])
-				frag2 = 1;
-		}
-	}
-	if (frag1 + frag2 <= 1)
-		printf("sorted");
-	else
-		printf("unsorted");
-	return 0;
+    int n = 0;
+    scanf("%d", &n);
+    int i = 0;
+    int max = 0;
+    int min = 100;
+    int score = 0;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &score);
+        if (score > max)
+        {
+            max = score;
+        }
+        if (score < min)
+        {
+            min = score;
+        }
+    }
+    printf("%d\n", max - min);
+    return 0;
 }
