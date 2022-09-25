0d78c6f477b9181e87e684f1f43d39be30e11e1b
Janonez <95027402+sharkmini@users.noreply.github.com>
2022-09-25 21:26:29 +0800

    c-language
    code

--- 练习/练习/test.c
+++ 练习/练习/test.c
@@ -345,3 +345,46 @@
 //    }
 //    return 0;
 //}
+
+
+// KiKi学习了循环，BoBo老师给他出了一系列打印图案的练习，该任务是打印用“*”组成的箭形图案。
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    while (scanf("%d", &n) != EOF)
+    {
+        int i = 0;
+        int j = 0;
+        // 上半部分
+        for (i = 0; i < n; i++)
+        {
+            // 打印空格
+            for (j = 0; j < n - i; j++)
+            {
+                printf("  ");
+            }
+            // 打印*
+            for (j = 0; j <= i; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+        // 下半部分
+        for (i = 0; i < n + 1; i++)
+        {
+            for (j = 0; j < i; j++)
+            {
+                printf("  ");
+            }
+            for (j = 0; j < n + 1 - i; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
