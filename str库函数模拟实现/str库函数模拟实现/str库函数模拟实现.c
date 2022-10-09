14d3a91a73a675f1789f02bdfab065477b6ae731
Janonez <95027402+sharkmini@users.noreply.github.com>
2022-10-10 00:24:04 +0800

    c-language
    code

--- str库函数模拟实现/str库函数模拟实现/str库函数模拟实现.c
+++ str库函数模拟实现/str库函数模拟实现/str库函数模拟实现.c
@@ -156,25 +156,63 @@
 
 // 6.模拟实现memcpy
 // void * memcpy ( void * des, const void * src, size_t num );
+//#include <stdio.h>
+//#include <assert.h>
+//void* my_memcpy ( void * des, const void * src, size_t num )
+//{
+//	void* ret = des;
+//	assert(des);
+//	assert(src);
+//	while (num--)
+//	{
+//		*(char*)des = *(char*)src;
+//		des = (char*)des + 1;
+//		src = (char*)src + 1;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[20]={0};
+//	char arr2[20] = "Hello World";
+//	printf("%s\n", my_memcpy(arr1, arr2, 11));
+//	return 0;
+//}
+
+
+// 7.模拟实现memmove
+// void * memmove ( void * destination, const void * source, size_t num )
 #include <stdio.h>
 #include <assert.h>
-void* my_memcpy ( void * des, const void * src, size_t num )
+void* my_memmove(void* des, const void* src, size_t num)
 {
-	void* ret = des;
 	assert(des);
 	assert(src);
-	while (num--)
+	void* ret = des;
+	if (des < src)
 	{
-		*(char*)des = *(char*)src;
-		des = (char*)des + 1;
-		src = (char*)src + 1;
+		while (num--)
+		{
+			*(char*)des = *(char*)src;
+			des = (char*)des + 1;
+			src = (char*)src + 1;
+		}
+	}
+	else
+	{
+		while (num--)
+		{
+			*((char*)des + num) = *((char*)src + num);
+		}
 	}
 	return ret;
 }
 int main()
 {
-	char arr1[20]={0};
-	char arr2[20] = "Hello World";
-	printf("%s\n", my_memcpy(arr1, arr2, 11));
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	my_memmove(arr + 2, arr, 20);
+	int i = 0;
+	for (i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
 	return 0;
 }
